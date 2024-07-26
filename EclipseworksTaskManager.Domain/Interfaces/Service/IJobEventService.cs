a6c23be23899a2107c383ac02f5cfad415a39ea3
Victor Oliveira <victorwork@yahoo.com.br>
2024-07-26 16:42:14 -0300

    removed unused service

--- EclipseworksTaskManager.Domain/Interfaces/Service/IJobEventService.cs
+++
@@ -1,9 +0,0 @@
-﻿using EclipseworksTaskManager.Domain.Entities;
-
-namespace EclipseworksTaskManager.Domain.Interfaces.Service
-{
-    public interface IJobEventService
-    {
-        Task AddAsync(JobEvent jobEvent);
-    }
-}
