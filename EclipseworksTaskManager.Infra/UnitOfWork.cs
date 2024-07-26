2c05681b92953656ca29469549b2856988bb9cbb
Victor Oliveira <victorwork@yahoo.com.br>
2024-07-26 15:47:13 -0300

    added unit of work implementation

---
+++ EclipseworksTaskManager.Infra/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using EclipseworksTaskManager.Domain.Interfaces;
+using EclipseworksTaskManager.Domain.Interfaces.Repository;
+using EclipseworksTaskManager.Infra.EntityConfig;
+
+namespace EclipseworksTaskManager.Infra
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public TaskManagerContext Context { get; set; }
+        public IJobRepository JobRepository { get; set; }
+
+        public IProjectRepository ProjectRepository { get; set; }
+
+        public IJobEventRepository JobEventRepository { get; set; }
+
+        public UnitOfWork(
+            TaskManagerContext taskManagerContext, 
+            IJobRepository jobRepository, 
+            IProjectRepository projectRepository, 
+            IJobEventRepository jobEventRepository)
+        {
+            JobRepository = jobRepository;
+            ProjectRepository = projectRepository;
+            JobEventRepository = jobEventRepository;
+            Context = taskManagerContext;
+        }
+
+        public Task SaveChangesAsync()
+        {
+            return Context.SaveChangesAsync();
+        }
+    }
+}
