051578493e7cbe0bf390682fc296ef7eacc3e339
Victor Oliveira <victorwork@yahoo.com.br>
2024-07-26 17:42:14 -0300

    added a new test to JobCommentService

--- EclipseworksTaskManager.UnitTests.Domain/Services/JobCommentServiceTests.cs
+++ EclipseworksTaskManager.UnitTests.Domain/Services/JobCommentServiceTests.cs
@@ -1,6 +1,8 @@
 ﻿using AutoFixture.Idioms;
+using EclipseworksTaskManager.Domain.Entities;
 using EclipseworksTaskManager.Domain.Services;
 using EclipseworksTaskManager.UnitTests.Domain.AutoFixture;
+using NSubstitute;
 using Xunit;
 
 namespace EclipseworksTaskManager.UnitTests.Domain.Services
@@ -12,6 +14,30 @@ namespace EclipseworksTaskManager.UnitTests.Domain.Services
         {
             assertion
                 .Verify(typeof(JobCommentService).GetConstructors());
+        }
+
+        [Theory, CustomAutoData]
+        public async Task AddAsync_ShouldAddCorrectly(
+            JobCommentService sut,
+            JobEvent jobEvent)
+        {
+            sut.UnitOfWork.JobEventRepository
+                .AddAsync(jobEvent)
+                .Returns(Task.CompletedTask);
+
+            sut.UnitOfWork
+                .SaveChangesAsync()
+                .Returns(Task.CompletedTask);
+
+            await sut.AddAsync(jobEvent);
+
+            await sut.UnitOfWork.JobEventRepository
+                .Received(1)
+                .AddAsync(jobEvent);
+
+            await sut.UnitOfWork
+                .Received(1)
+                .SaveChangesAsync();
         }
     }
 }
