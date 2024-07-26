ea455dd10970efe54de05fbcab06f984ce369e4f
Victor Oliveira <victorwork@yahoo.com.br>
2024-07-26 15:46:00 -0300

    added user service

---
+++ EclipseworksTaskManager.Domain/Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using EclipseworksTaskManager.Domain.Interfaces.Service;
+
+namespace EclipseworksTaskManager.Domain.Services
+{
+    public class UserService : IUserService
+    {
+        private string _user;
+
+        public string Get()
+            => _user;
+
+        public void Set(string user)
+            => _user = user;
+    }
+}
