a49a7e5e94c63674d51f54e578dfd51b29c5ea53
Victor Oliveira <victorwork@yahoo.com.br>
2024-07-26 15:46:25 -0300

    added job comment service

---
+++ EclipseworksTaskManager.Domain/Services/JobCommentService.cs
@@ -0,0 +1,23 @@
+﻿using EclipseworksTaskManager.Domain.Entities;
+using EclipseworksTaskManager.Domain.Interfaces;
+using EclipseworksTaskManager.Domain.Interfaces.Service;
+
+namespace EclipseworksTaskManager.Domain.Services
+{
+    public class JobCommentService : IJobCommentService
+    {
+        public IUnitOfWork UnitOfWork { get; set; }
+
+        public JobCommentService(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        public async Task AddAsync(JobEvent jobEvent)
+        {
+            await UnitOfWork.JobEventRepository.AddAsync(jobEvent);
+
+            await UnitOfWork.SaveChangesAsync();
+        }
+    }
+}
