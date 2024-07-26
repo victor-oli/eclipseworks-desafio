e6cbbbf0f71dc98a0475e7d8d932734e6841b0fa
Victor Oliveira <victorwork@yahoo.com.br>
2024-07-26 15:49:15 -0300

    added job event repository

---
+++ EclipseworksTaskManager.Infra/Repositories/JobEventRepository.cs
@@ -0,0 +1,23 @@
+﻿using EclipseworksTaskManager.Domain.Entities;
+using EclipseworksTaskManager.Domain.Interfaces.Repository;
+using EclipseworksTaskManager.Infra.EntityConfig;
+
+namespace EclipseworksTaskManager.Infra.Repositories
+{
+    public class JobEventRepository : IJobEventRepository
+    {
+        public TaskManagerContext Context { get; set; }
+
+        public JobEventRepository(TaskManagerContext context)
+        {
+            Context = context;
+        }
+
+        public async Task AddAsync(JobEvent jobEvent)
+        {
+            jobEvent.Id = Guid.NewGuid();
+
+            await Context.JobEvents.AddAsync(jobEvent);
+        }
+    }
+}
