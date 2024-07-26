3b010aaaa9715a026eac4a899c462d7d15c5a083
Victor Oliveira <victorwork@yahoo.com.br>
2024-07-26 15:49:36 -0300

    added api project

---
+++ EclipseworksTaskManager.Api/Program.cs
@@ -0,0 +1,52 @@
+using EclipseworksTaskManager.Domain.Interfaces;
+using EclipseworksTaskManager.Domain.Interfaces.Repository;
+using EclipseworksTaskManager.Domain.Interfaces.Service;
+using EclipseworksTaskManager.Domain.Services;
+using EclipseworksTaskManager.Infra;
+using EclipseworksTaskManager.Infra.EntityConfig;
+using EclipseworksTaskManager.Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<TaskManagerContext>(x =>
+//    x.UseInMemoryDatabase("TaskManager"));
+
+builder.Services.AddDbContext<TaskManagerContext>(x =>
+    x.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), b =>
+    {
+        b.MigrationsAssembly("EclipseworksTaskManager.Api");
+        b.EnableRetryOnFailure(3, TimeSpan.FromSeconds(5), null);
+    }));
+
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+builder.Services.AddTransient<IJobRepository, JobRepository>();
+builder.Services.AddTransient<IProjectRepository, ProjectRepository>();
+builder.Services.AddTransient<IJobEventRepository, JobEventRepository>();
+
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<IJobService, JobService>();
+builder.Services.AddTransient<IProjectService, ProjectService>();
+builder.Services.AddTransient<IJobCommentService, JobCommentService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
